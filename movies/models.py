6526d806a93d21a354a3cf5ea074ef76dd78affa
griminir <viktor.elliot.king@gmail.com>
2024-11-06 13:57:14 +0100

    set up admin for adding movies and genres

--- movies/models.py
+++ movies/models.py
@@ -2,8 +2,13 @@ from django.db import models
 from django.utils import timezone
 
 # Create your models here.
+
+
 class Genre(models.Model):
     name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return str(self.name)
 
 
 class Movie(models.Model):
