995c518623052afd3c84b7c6edafffa443ce2dea
griminir <viktor.elliot.king@gmail.com>
2024-11-06 14:52:15 +0100

    made first template, installed django for vsc to get django html, added pylintrc file for linting django code

--- movies/views.py
+++ movies/views.py
@@ -1,8 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from .models import Movie
 # Create your views here. Are like controllers in MVC
 
 
 def index(request):
-    return HttpResponse('Hello, World!')
+    movies = Movie.objects.all()
+    # select * from movies_movie
+    # Movie.objects.filter(release_year=2013)
+    # select * from movies_movie where release_year = 2013
+    return render(request, 'index.html', {'movies': movies})
