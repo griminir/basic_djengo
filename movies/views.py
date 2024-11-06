2d8620ea062de1d0d28a7f43979018b3c27eb90d
griminir <viktor.elliot.king@gmail.com>
2024-11-06 14:56:43 +0100

    improved pathing because of how django looks for files when looking for html files

--- movies/views.py
+++ movies/views.py
@@ -9,4 +9,4 @@ def index(request):
     # select * from movies_movie
     # Movie.objects.filter(release_year=2013)
     # select * from movies_movie where release_year = 2013
-    return render(request, 'index.html', {'movies': movies})
+    return render(request, 'movies/index.html', {'movies': movies})
