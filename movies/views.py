119e88cf1a33870b4cc1c2e0c269d15a1a7b41e3
griminir <viktor.elliot.king@gmail.com>
2024-11-07 11:42:17 +0100

    making a details page for movies and using the 404 shortcut

--- movies/views.py
+++ movies/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
 from .models import Movie
 # Create your views here. Are like controllers in MVC
 
@@ -10,3 +10,9 @@ def index(request):
     # Movie.objects.filter(release_year=2013)
     # select * from movies_movie where release_year = 2013
     return render(request, 'movies/index.html', {'movies': movies})
+
+
+def detail(request, movie_id):
+    # can also use primary key as pk instead of id
+    movie = get_object_or_404(Movie, id=movie_id)
+    return render(request, 'movies/detail.html', {'movie': movie})
